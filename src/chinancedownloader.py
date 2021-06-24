3087a8e92e31e57b49d78a5c223a4e54117894b8
Alex Potapchuk <a.v.potapchuk@yandex.com>
2021-06-24 16:35:43 +0300

    Refactoring

--- src/chinancedownloader.py
+++ src/chinancedownloader.py
@@ -2,6 +2,7 @@ from moex import MOEX
 import datetime
 from datetime import date
 from urllib import request
+from pandas.core.frame import DataFrame
 from yahoofinancials import YahooFinancials
 import pandas as pd
 import os
@@ -9,115 +10,85 @@ import investpy
 from pathlib import Path
 
 
-def update_data(source: str, country: str, ticket: str, date_from: date, date_to: date, filename: str, currency: str = 'UNK'):
+def get_or_update_data(source: str, country: str, ticket: str, date_from: date, date_to: date, filename: str, currency: str = 'UNK'):
 
+    data = DataFrame()
     if source is 'moex':
-        update_from_moex(filename=filename,
-                         ticket=ticket, date_from=date_from, date_to=date_to)
+        data = get_data_from_moex(
+            ticket=ticket, date_from=date_from, date_to=date_to)
     elif source is 'stooq':
-        update_from_stooq(filename=filename,
-                          ticket=ticket, date_from=date_from, date_to=date_to, currency=currency)
+        data = get_data_from_stooq(
+            ticket=ticket, date_from=date_from, date_to=date_to, currency=currency)
     elif source is 'msci':
-        update_from_msci(filename=filename,
-                         ticket=ticket, date_from=date_from, date_to=date_to)
+        data = get_data_from_msci(
+            ticket=ticket, date_from=date_from, date_to=date_to)
     elif source is 'yahoo':
-        update_from_yahoo(filename=filename,
-                          ticket=ticket, date_from=date_from, date_to=date_to, currency=currency)
+        data = get_data_from_yahoo(
+            ticket=ticket, date_from=date_from, date_to=date_to, currency=currency)
     elif source is 'investing':
-        update_from_investing(filename=filename, country=country,
-                              ticket=ticket, date_from=date_from, date_to=date_to)
+        data = get_data_from_investing(ticket=ticket, country=country,
+                                       date_from=date_from, date_to=date_to)
     else:
         raise ValueError('Unknown data source')
+
+    data = set_good_look(data)
+    save_to_csv(data, filename=filename)
 
     return filename + '.csv'
 
 
-def update_from_investing(filename: str, country: str,  ticket: str, date_from: date, date_to: date):
-    df = investpy.get_index_historical_data(index=ticket,
-                                            country=country,
-                                            from_date=date_from.strftime(
-                                                '%d/%m/%Y'),
-                                            to_date=date_to.strftime('%d/%m/%Y'))
+def get_data_from_investing(ticket: str, country: str, date_from: date, date_to: date):
+    data = investpy.get_index_historical_data(index=ticket,
+                                              country=country,
+                                              from_date=date_from.strftime(
+                                                  '%d/%m/%Y'),
+                                              to_date=date_to.strftime('%d/%m/%Y'))
 
-    df.reset_index(inplace=True)
-    df.rename(columns={'Date': 'date', 'Open': 'open',
-                       'High': 'high', 'Low': 'low',
-                       'Close': 'close', 'Volume': 'volume',
-                       'Currency': 'currency', }, inplace=True)
+    data.reset_index(inplace=True)
+    data.rename(columns={'Date': 'date', 'Open': 'open',
+                         'High': 'high', 'Low': 'low',
+                         'Close': 'close', 'Volume': 'volume',
+                         'Currency': 'currency', }, inplace=True)
 
-    if 'capitalization' not in df.columns:
-        df = df.assign(capitalization=0.0)
+    data = data.assign(secid='investing:' + ticket)
 
-    df = df.assign(secid='investing:' + ticket)
-
-    df = df[['secid', 'currency', 'date', 'close', 'open',
-             'low', 'high', 'volume', 'capitalization']]
-
-    df.set_index(['date'], inplace=True)
-
-    path_to_file = '..\\data\\{}.csv'.format(filename)
-    df.to_csv(path_to_file,
-              mode='a', header=(not Path(path_to_file).is_file()))
+    return data
 
 
-def update_from_moex(filename: str, ticket: str, date_from: date, date_to: date):
+def get_data_from_moex(ticket: str, date_from: date, date_to: date):
     moex = MOEX()
     data = moex.history_engine_market_security(
         date_start=date_from.strftime('%Y-%m-%d'), date_end=date_to.strftime('%Y-%m-%d'), security=ticket)
 
-    df = data[["TRADEDATE", "SECID", "OPEN", "CLOSE",
-               "LOW", "HIGH", "VALUE", "CURRENCYID", 'CAPITALIZATION']]
+    data = data[["TRADEDATE", "SECID", "OPEN", "CLOSE",
+                 "LOW", "HIGH", "VALUE", "CURRENCYID", 'CAPITALIZATION']]
 
-    df.rename(columns={'TRADEDATE': 'date', 'OPEN': 'open',
-                       'HIGH': 'high', 'LOW': 'low', 'CLOSE': 'close',
-                       'SECID': 'secid',  'VALUE': 'volume',
-                       'CURRENCYID': 'currency',  'CAPITALIZATION': 'capitalization', }, inplace=True)
-
-    df = df[['secid', 'currency', 'date', 'close', 'open',
-             'low', 'high', 'volume', 'capitalization']]
-
-    df.set_index(['date'], inplace=True)
-
-    path_to_file = '..\\data\\{}.csv'.format(filename)
-    df.to_csv(path_to_file,
-              mode='a', header=(not Path(path_to_file).is_file()))
+    data.rename(columns={'TRADEDATE': 'date', 'OPEN': 'open',
+                         'HIGH': 'high', 'LOW': 'low', 'CLOSE': 'close',
+                         'SECID': 'secid',  'VALUE': 'volume',
+                         'CURRENCYID': 'currency',  'CAPITALIZATION': 'capitalization', }, inplace=True)
+    return data
 
 
-def update_from_stooq(filename: str, ticket: str, date_from: date, date_to: date, currency='UNK'):
-    date_start = date_from.strftime('%Y%m%d')
-    date_end = date_to.strftime('%Y%m%d')
+def get_data_from_stooq(ticket: str, date_from: date, date_to: date, currency='UNK'):
     url = 'https://stooq.com/q/d/l/?s={0}&d1={1}&d2={2}&i=d'.format(
-        ticket, date_start, date_end)
+        ticket, date_from.strftime('%Y%m%d'), date_to.strftime('%Y%m%d'))
 
-    df = pd.read_csv(url)
-    df.rename(columns={'Date': 'date', 'Open': 'open',
-                       'High': 'high', 'Low': 'low',
-                       'Close': 'close', 'Volume': 'volume'}, inplace=True)
+    data = pd.read_csv(url)
+    data.rename(columns={'Date': 'date', 'Open': 'open',
+                         'High': 'high', 'Low': 'low',
+                         'Close': 'close', 'Volume': 'volume'}, inplace=True)
 
-    if 'volume' not in df.columns:
-        df = df.assign(volume=0.0)
+    data = data.assign(
+        currency='USD',
+        secid='STOOQ:' + ticket)
 
-    if 'capitalization' not in df.columns:
-        df = df.assign(capitalization=0.0)
-
-    df = df.assign(currency='USD', secid='STOOQ:' + ticket)
-
-    df = df[['secid', 'currency', 'date', 'close', 'open',
-             'low', 'high', 'volume', 'capitalization']]
-
-    df.set_index(['date'], inplace=True)
-
-    path_to_file = '..\\data\\{}.csv'.format(filename)
-    df.to_csv(path_to_file,
-              mode='a', header=(not Path(path_to_file).is_file()))
+    return data
 
 
-def update_from_msci(filename: str, ticket: str, date_from: date, date_to: date):
-    date_start = date_from.strftime('%Y%m%d')
-    date_end = date_to.strftime('%Y%m%d')
+def get_data_from_msci(ticket: str, date_from: date, date_to: date):
     url = 'https://app2.msci.com/products/service/index/indexmaster/downloadLevelData?output=INDEX_LEVELS&currency_symbol=USD&index_variant=STRD&start_date={0}&end_date={1}&data_frequency=DAILY&baseValue=false&index_codes={2}'.format(
-        date_start, date_end, ticket)
-    print(url)
+        date_from.strftime('%Y%m%d'), date_to.strftime('%Y%m%d'), ticket)
     content = request.urlopen(url).read()
     temp = content
     with open('..\\data\\msci_temp.xls', 'wb+') as fp:
@@ -127,46 +98,54 @@ def update_from_msci(filename: str, ticket: str, date_from: date, date_to: date)
     xls = xls.drop(xls.index[[0, 1, 2, 3, 4, 5]])
     xls = xls.drop(
         xls.index[list(range(len(xls.index) - 19, len(xls.index)))])
-    df = pd.DataFrame({'date': [datetime.datetime.strptime(d, "%b %d, %Y").date() for d in xls['Unnamed: 0'].values],
-                       'secid':         'MSCI:' + filename,
-                       'close':          xls['Unnamed: 1'].values,
-                       'open':           0.0,
-                       'low':            0.0,
-                       'high':           0.0,
-                       'volume':         0.0,
-                       'capitalisation': 0.0,
-                       'currency':      'USD'})
-
-    df = df[['secid', 'currency', 'date', 'close', 'open',
-             'low', 'high', 'volume', 'capitalization']]
-
-    df.set_index(['date'], inplace=True)
-
-    path_to_file = '..\\data\\{}.csv'.format(filename)
-    df.to_csv(path_to_file,
-              mode='a', header=(not Path(path_to_file).is_file()))
+    data = pd.DataFrame({'date': [datetime.datetime.strptime(d, "%b %d, %Y").date() for d in xls['Unnamed: 0'].values],
+                         'secid':         'MSCI:' + ticket,
+                         'close':          xls['Unnamed: 1'].values,
+                         'open':           0.0,
+                         'low':            0.0,
+                         'high':           0.0,
+                         'volume':         0.0,
+                         'capitalisation': 0.0,
+                         'currency':      'USD'})
 
     if(os.path.isfile('..\\data\\msci_temp.xls')):
         os.remove('..\\data\\msci_temp.xls')
 
+    return data
 
-def update_from_yahoo(filename: str, ticket: str, date_from: date, date_to: date, currency='UNK'):
+
+def get_data_from_yahoo(ticket: str, date_from: date, date_to: date, currency='UNK'):
     yahoo_financials = YahooFinancials(ticket)
     historical_stock_prices = yahoo_financials.get_historical_price_data(
         date_from.strftime('%Y-%m-%d'), date_to.strftime('%Y-%m-%d'), 'daily')
 
-    df = pd.DataFrame(historical_stock_prices[ticket]['prices'])
-    df = df.drop(['date', 'adjclose'], axis=1)
-    df = df.rename(columns={'formatted_date': 'date'})
+    data = pd.DataFrame(historical_stock_prices[ticket]['prices'])
+    data = data.drop(['date', 'adjclose'], axis=1)
+    data = data.rename(columns={'formatted_date': 'date'})
 
-    df = df.assign(capitalization=0.0,
-                   currency=historical_stock_prices[ticket]['currency'], secid='yahoo:' + ticket)
+    data = data.assign(
+        currency=historical_stock_prices[ticket]['currency'],
+        secid='yahoo:' + ticket)
 
-    df = df[['secid', 'currency', 'date', 'close', 'open',
-             'low', 'high', 'volume', 'capitalization']]
+    return data
 
-    df.set_index(['date'], inplace=True)
 
+def set_good_look(dataframe: DataFrame):
+    if 'volume' not in dataframe.columns:
+        dataframe = dataframe.assign(volume=0.0, inplace=True)
+
+    if 'capitalization' not in dataframe.columns:
+        dataframe = dataframe.assign(capitalization=0.0, inplace=True)
+
+    dataframe = dataframe[['secid', 'currency', 'date', 'close', 'open',
+                           'low', 'high', 'volume', 'capitalization']]
+
+    dataframe.set_index(['date'], inplace=True)
+
+    return dataframe
+
+
+def save_to_csv(dataframe: DataFrame, filename: str):
     path_to_file = '..\\data\\{}.csv'.format(filename)
-    df.to_csv(path_to_file,
-              mode='a', header=(not Path(path_to_file).is_file()))
+    dataframe.to_csv(path_to_file,
+                     mode='a', header=(not Path(path_to_file).is_file()))
